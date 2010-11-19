712d702b29db68f78f8272be075b3824be6d0f80
Edward <eddy771@berkeley.edu>
2010-11-19 15:25:26 -0800

    landing page works

---
+++ fitness/app/controllers/landing_controller.rb
@@ -0,0 +1,7 @@
+class LandingController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html
+    end
+  end
+end
--- fitness/config/routes.rb
+++ fitness/config/routes.rb
@@ -1,9 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
-	map.resource :account, :controller => "users"
   map.resources :users
+  
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new"
+  
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
   # Sample of regular route:
@@ -43,6 +43,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+  map.root :controller => "landing", :action => "index"
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
