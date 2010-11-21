b10680795144ab8fd4720ce76504af95dd5164c4
Edward <eddy771@berkeley.edu>
2010-11-21 15:11:43 -0800

    modified migrations

--- fitness/db/migrate/20101120001926_create_exercises.rb
+++ fitness/db/migrate/20101120001926_create_exercises.rb
@@ -2,7 +2,7 @@ class CreateExercises < ActiveRecord::Migration
   def self.up
     create_table :exercises do |t|
       t.string :name
-      t.string :type
+      t.string :exercise_type
 
       t.timestamps
     end
