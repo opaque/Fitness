979e41d52484e3cb2ed54ca05904c26d302ff0a3
Edward <eddy771@berkeley.edu>
2010-11-22 01:44:12 -0800

    added associations

--- fitness/app/models/event.rb
+++ fitness/app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   has_many :workout_sessions
   has_one :user, :through => :workout_sessions
   has_many :exercises, :through => :workout_sessions
+  has_many :workout_histories, :through => :workout_sessions
 end
