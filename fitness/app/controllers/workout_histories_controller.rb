999ac1b4d023e0cc2971385e8b3248402e2cebdf
Stephanie Ren <stephanieren@berkeley.edu>
2010-11-22 02:37:48 -0800

    graphs phase 1

--- fitness/app/controllers/workout_histories_controller.rb
+++ fitness/app/controllers/workout_histories_controller.rb
@@ -1,12 +1,30 @@
+require 'gchart'
+
 class WorkoutHistoriesController < ApplicationController
   # GET /workout_histories
   # GET /workout_histories.xml
   def index
     @workout_histories = WorkoutHistory.all
+	
+	@actual_data = Array.new
+	@estimated_data = Array.new
+	
+	@workout_histories.each do |workout_history|
+		@actual_data.push(workout_history[:actual_reps])
+		@workout_sess = WorkoutSession.find(workout_history[:workout_session_id])
+		@estimated_data.push(@workout_sess[:estimated_reps])
+	end
+	
+	@g = Gchart.line(:title => "Actual vs Estimated Reps",
+           :data => [@actual_data, @estimated_data], 
+           :line_colors => 'FF0000,00FF00',
+		   :legend => ["Actual Reps", "Estimate Reps"],
+		   :axis_with_labels => ['x', 'y', 'r'])
 
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @workout_histories }
+	  format.xml  { render :xml => @g }
     end
   end
 
@@ -17,6 +35,20 @@ class WorkoutHistoriesController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
+      format.xml  { render :xml => @workout_history }
+    end
+  end
+  
+  def graph
+    @workout_history = WorkoutHistory.new
+	Gchart.line(:size => '200x300', 
+				:title => "example title",
+				:bg => 'efefef',
+				:legend => ['first data set label', 'second data set label'],
+				:data => [10, 30, 120, 45, 72])
+	
+	respond_to do |format|
+      format.html # graph.html.erb
       format.xml  { render :xml => @workout_history }
     end
   end
@@ -80,4 +112,6 @@ class WorkoutHistoriesController < ApplicationController
       format.xml  { head :ok }
     end
   end
+  
+
 end
