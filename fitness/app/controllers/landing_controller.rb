24979208923a0c1b99f6f96053f72077415eb635
David Chock <david.chock@gmail.com>
2010-11-23 05:10:19 -0800

    removelanding

--- fitness/app/controllers/landing_controller.rb
+++ fitness/app/controllers/landing_controller.rb
@@ -1,7 +1,3 @@
 class LandingController < ApplicationController
-  def index
-    respond_to do |format|
-      format.html
-    end
-  end
+
 end
---
+++ fitness/spec/controllers/landing_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+
+describe LandingController do
+  describe "GET index" do
+    it "assigns all landings as @landings" do
+    end
+  end
+end
