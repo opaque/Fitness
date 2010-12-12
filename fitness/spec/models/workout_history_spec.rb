fa28bcd9da77ecb37555e493aea40540ef92d243
David Chock <david.chock@gmail.com>
2010-12-12 02:20:18 -0800

    editspec

--- fitness/spec/models/workout_history_spec.rb
+++ fitness/spec/models/workout_history_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe WorkoutHistory do
   before(:each) do
     @valid_attributes = {
-      :actual_reps = 10,
-	  :actual_sets = 10,
-	  :calories = nil,
-	  :workout_session_id = 1
+      :actual_reps => 10,
+	  :actual_sets => 10,
+	  :calories => nil,
+	  :workout_session_id => 1
     }
 	
   end
@@ -36,37 +36,37 @@ describe WorkoutHistory do
 	
 	before(:each) do
 	  @invalid_attributes = {
-      :actual_reps = 10,
-	  :actual_sets = 10,
-	  :calories = nil,
-	  :workout_session_id = 1
+      :actual_reps => 10,
+	  :actual_sets => 10,
+	  :calories => nil,
+	  :workout_session_id => 1
       }
 	  
 	end
   
 	  it "should not create a new instance given non numerical rep values" do
 		@invalid_attributes[:actual_reps] = "fun",
-		WorkoutHistory.create(@invalid_attributes).should be_false
+		WorkoutHistory.create(@invalid_attributes).should == false
 	  end
 	  
 	  it "should not create a new instance given non numerical set values" do
 		@invalid_attributes[:actual_sets] = "fail set"
-		WorkoutHistory.create(@invalid_attributes).should be_false
+		WorkoutHistory.create(@invalid_attributes).should == false
 	  end
 	  
 	  it "should not create a new instance given non numerical caloric values" do
 		@invalid_attributes[:calories] = "fail calories"
-		WorkoutHistory.create(@invalid_attributes).should be_false
+		WorkoutHistory.create(@invalid_attributes).should == false
 	  end
 	  
 	  it "should not create a new instance if a workout history has already been created for specified session" do
 		WorkoutHistory.create(@valid_attributes)
-		WorkoutHistory.create(@invalid_attributes).should be_false
+		WorkoutHistory.create(@invalid_attributes).should == false
 	  end
 	  
 	  it "should not create a new instance given null workout session id association" do
 		@invalid_attributes.delete(:workout_session_id)
-		WorkoutHistory.create(@invalid_attributes).should be_false
+		WorkoutHistory.create(@invalid_attributes).should == false
 	  end
 	  
   
