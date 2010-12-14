12960433cde81ed0eab67701e9e4b35479d80b00
Edward <eddy771@berkeley.edu>
2010-12-13 17:21:59 -0800

    hopefully changes coverage

--- fitness/spec/controllers/landing_controller_spec.rb
+++ fitness/spec/controllers/landing_controller_spec.rb
@@ -5,7 +5,19 @@ describe LandingController do
   describe "handling GET index" do
 	it "should render the index template" do
 	  get :index
-	  controller.should render_template("index" )
+	  controller.should render_template("index")
+	end
+	it "should render the menu_one partial" do
+	  get :menu_one
+	  controller.should render_template("landing/_menu1")
+	end
+	it "should render the menu_two partial" do
+	  get :menu_one
+	  controller.should render_template("landing/_menu2")
+	end
+	it "should render the menu_two partial" do
+	  get :menu_one
+	  controller.should render_template("landing/_menu3")
 	end
   end
 end
